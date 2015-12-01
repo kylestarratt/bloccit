2d996e7f56f3b5d370741df48e0507c57c9769ea
Kyle Starratt <kyle@rentitez.com>
2015-12-01 12:03:41 -0700

    finally done with 40

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,9 +1,9 @@
 module TopicsHelper
-  def user_is_authorized_for_topics?(topic, action)
-    if action == "destroy"
-      current_user == post.user || current_user.admin?
+  def user_is_authorized_for_topics?(action, topic=nil)
+    if action == "destroy" || action == 'new'
+      current_user.admin?
     elsif action == "edit" || action == "update"
-      current_user == post.user || current_user.admin? || current_user.moderator?
+      current_user.admin? || current_user.moderator?    
     end
   end
 end
