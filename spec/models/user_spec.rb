05127e90d3780cdccb02adfb3ecce723fdecbe12
Kyle Starratt <kyle@rentitez.com>
2015-12-07 10:04:19 -0700

    working on checkpoint 45

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { create(:user) }
   it { should have_many(:posts)}
   it { should have_many(:comments) }
   it { should have_many(:votes) }
@@ -99,7 +99,7 @@ RSpec.describe User, type: :model do
        topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
        @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
      end
- 
+
      it "returns `nil` if the user has not favorited the post" do
        expect(user.favorite_for(@post)).to be_nil
      end
@@ -107,6 +107,15 @@ RSpec.describe User, type: :model do
      it "returns the appropriate favorite if it exists" do
        favorite = user.favorites.where(post: @post).create
        expect(user.favorite_for(@post)).to eq(favorite)
+     end
+   end
+
+   describe ".avatar_url" do
+     let(:known_user) { create(:user, email: "blochead@bloc.io") }
+
+     it "returns the proper Gravatar url for a known email entity" do
+       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
      end
    end
 end
