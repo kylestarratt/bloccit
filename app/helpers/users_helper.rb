534cd6dee67a2cc682b55c0947eaa5e07c614a8c
Kyle Starratt <kyle@rentitez.com>
2016-01-07 10:53:45 -0700

    Assignment 45 is all done

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def check_for_something(what)
+    if current_user.send(what).present?
+      "<h2>#{what.capitalize}</h2>".html_safe
+    else
+      "<h4>#{current_user.name} has not submitted any #{what} yet.</h4>".html_safe
+    end
+  end
+
 end
