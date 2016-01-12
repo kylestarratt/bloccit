02e00236b5e0278a289c0fd734ac8992075dfd2c
Kyle Starratt <kyle@rentitez.com>
2016-01-12 11:18:49 -0700

    checkpoint 47 is all done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,9 +5,11 @@ class Comment < ActiveRecord::Base
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
 
+  default_scope { order('updated_at DESC') }
+
   after_create :send_favorite_emails
 
-   private
+  private
 
    def send_favorite_emails
      post.favorites.each do |favorite|
