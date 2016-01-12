82539fdd923ccd007bb29dc50a023e40a99cb4a7
Kyle Starratt <kyle@rentitez.com>
2016-01-12 16:42:34 -0700

    checkpoint 48 is done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,7 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:email)
     end
   end
+
   describe "invalid user" do
     let(:user_with_invalid_name) { build(:user, name: "") }
     let(:user_with_invalid_email) { build(:user, email: "") }
@@ -95,27 +96,33 @@ RSpec.describe User, type: :model do
   end
 
   describe "#favorite_for(post)" do
-     before do
-       topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
-       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
-     end
+    before do
+      topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+    end
 
-     it "returns `nil` if the user has not favorited the post" do
-       expect(user.favorite_for(@post)).to be_nil
-     end
+    it "returns `nil` if the user has not favorited the post" do
+      expect(user.favorite_for(@post)).to be_nil
+    end
 
-     it "returns the appropriate favorite if it exists" do
-       favorite = user.favorites.where(post: @post).create
-       expect(user.favorite_for(@post)).to eq(favorite)
-     end
-   end
+    it "returns the appropriate favorite if it exists" do
+      favorite = user.favorites.where(post: @post).create
+      expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  end
 
-   describe ".avatar_url" do
-     let(:known_user) { create(:user, email: "blochead@bloc.io") }
+  describe ".avatar_url" do
+    let(:known_user) { create(:user, email: "blochead@bloc.io") }
 
-     it "returns the proper Gravatar url for a known email entity" do
-       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
-       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
-     end
-   end
+    it "returns the proper Gravatar url for a known email entity" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
+    end
+  end
+
+  describe "#generate_auth_token" do
+    it "creates a token" do
+      expect(user.auth_token).to_not be_nil
+    end
+  end
 end
